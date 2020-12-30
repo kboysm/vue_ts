ed4b5dde426d24d4ea22af1b91a331f871fef9bd
kboysm <lishal12@naver.com>
2020-12-30 22:14:21 +0900

    조건문과 레이아웃 변경

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <layout-normal />
+  <layout-normal v-if="isAuth" />
+  <layout-auth v-else />
 </template>
 <script lang="ts">
 import { Vue ,Component} from "vue-property-decorator"
 import LayoutNormal from "@/components/layout/normal.vue"
+import LayoutAuth from "@/components/layout/auth.vue"
 
 @Component({
   components:{
-    LayoutNormal
+    LayoutNormal , LayoutAuth
   }
 })
 export default class App extends Vue {
-  
+  isAuth = false
 }
 
 </script>
---
+++ src/components/layout/auth.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-app>
+        <v-main>
+            <v-container class="justify-center align-center">
+                <v-text-field></v-text-field>
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class LayoutAuth extends Vue {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
